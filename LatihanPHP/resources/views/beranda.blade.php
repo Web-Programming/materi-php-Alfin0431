f5b979d4528695abb85209709589aed8dc277191
Alfin0431 <alfinnn831@gmail.com>
2025-04-29 20:32:39 +0700

    pembelajaran code awal (PHP)
    membuat method, dan menampilkan tampilan view

---
+++ LatihanPHP/resources/views/beranda.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1> Ini Halaman Beranda </h1>
+    <h2> Hallo {{ $name }}</h2>
+    <h2> Your Email : {{ $email }}</h2>
+    <h2> Alamat : {{ $alamat }}</h2>
+    <!-- An unexamined life is not worth living. - Socrates -->
+</div>
--- LatihanPHP/routes/web.php
+++ LatihanPHP/routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Pest\Arch\Objects\FunctionDescription;
+use PhpParser\Builder\Function_;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view(
+        'beranda',
+        [
+            'name' => 'Alfin',
+            'email' => 'alfinnn831@gmail.com',
+            'alamat' => 'Jl. Jalanin Aja'
+        ]
+    );
+});
+
+Route::get('/berita/{id}/{judul?}', function ($id, $judul = null) {
+    return view(
+        'berita',
+        ['id' => $id, 'judul' => $judul]
+    );
+
+});
+
+
