2d1665fa8631a06675a470fcc7b2882247358de1
Alfin0431 <alfinnn831@gmail.com>
2025-05-06 20:32:01 +0700

    Migration & Querry Builder

--- LatihanPHP/database/seeders/DatabaseSeeder.php
+++ LatihanPHP/database/seeders/DatabaseSeeder.php
@@ -5,7 +5,10 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use DB;
+use Dotenv\Parser\Value;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Email;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +18,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        DB::table('users')
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+            'name' => 'Alfin',
+            'email' => 'alfinnn831@gmail.com',
+            'password' => Hash::make(value: 'password')
         ]);
+
+        DB::table('users')
+            ->where(column: 'id', operator: 1)
+            ->update(values: [
+                'password' => Hash::make('123456')
+            ]);
     }
 }
